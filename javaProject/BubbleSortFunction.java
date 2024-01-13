74ed93d1a443eecf83e41319ffd1922cd8ee8b45
Noemí García <124080941+uo288455@users.noreply.github.com>
2024-01-13 21:38:46 +0100

    Update BubbleSortFunction.java

--- javaProject/BubbleSortFunction.java
+++ javaProject/BubbleSortFunction.java
@@ -15,21 +15,31 @@ public class BubbleSortFunction {
 			throw new IllegalArgumentException("Invalid input array");
 		}
 		
+		boolean swapped;
+		
 		// Create a copy of the input array to avoid modifying it
 		List<Integer> array = new ArrayList<>(inputArray);
 		
 		for (int i = 0; i < array.size(); i++) {
+			
+			// Boolean variable is initialized to false so that it is then checked if there was another swap
+			swapped = false;
+			
 			for (int j = 0; j < array.size() - 1; j++) {
 				if (array.get(j) > array.get(j+1)) {
 					// Swap array[j] and array[j+1]
 					int aux = array.get(j);
 					array.set(j, array.get(j+1));
 					array.set(j+1, aux);
+					swapped = true;
 				}
+			}
+			
+			if(!swapped) {
+				break;
 			}
 		}
 		
 		return array;
 	}
-
 }
